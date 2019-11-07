984a733c4c92bab38c8574708388e8b508be8f01
Syed Sirajul Islam Anik <sirajul.islam.anik@gmail.com>
2019-11-07 17:56:23 +0600

    tutorial7 added

--- polls/models.py
+++ polls/models.py
@@ -15,9 +15,13 @@ class Question(models.Model):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published Recently?'
+
 
 class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, null=True, on_delete=models.SET_NULL)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
 
